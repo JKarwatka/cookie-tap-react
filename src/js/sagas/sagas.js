13ce0ad871382b1150a512ad4872f348412b348b
Jakub Karwatka <jakubkarwatka@gmail.com>
2019-04-14 21:59:21 +0200

    refactor sagas

--- src/js/sagas/sagas.js
+++ src/js/sagas/sagas.js
@@ -1,15 +1,46 @@
-import { put, select, all, delay } from 'redux-saga/effects';
+import {
+  put,
+  select,
+  all,
+  delay,
+  take,
+  fork,
+  takeEvery,
+  call
+} from 'redux-saga/effects';
 import { cookiesPerSecondSelector } from '../selectors/selectors';
-import { addCookie } from '../actions/actions';
+import { addCookie, generateCookiesFromProducers } from '../actions/actions';
+import { GENERATE_COOKIE_FROM_PRODUCERS } from '../actions/actionTypes';
 
 function* addCookiesFromProducers() {
+  const cookiesPerSecond = yield select(cookiesPerSecondSelector);
+  yield put(addCookie(cookiesPerSecond / 20));
+}
+
+function* watchGenerateCookiesFromProducers() {
   while (true) {
-    yield delay(10);
-    const cookiesPerSecond = yield select(cookiesPerSecondSelector);
-    yield put(addCookie(cookiesPerSecond / 100));
+    yield take(GENERATE_COOKIE_FROM_PRODUCERS);
+    yield fork(addCookiesFromProducers);
+  }
+}
+
+function* dispatchGenerateCookiesFromProducers() {
+  while (true) {
+    yield delay(50);
+    yield put(generateCookiesFromProducers());
+  }
+}
+
+function* dispatchSaveState() {
+  while (true) {
+    yield delay(50);
+    yield put(generateCookiesFromProducers());
   }
 }
 
 export default function* rootSaga() {
-  yield all([addCookiesFromProducers()]);
+  yield all([
+    fork(dispatchGenerateCookiesFromProducers),
+    fork(watchGenerateCookiesFromProducers)
+  ]);
 }
