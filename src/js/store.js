d48a1bad1263ac116bb12508591b3a306d96fa06
Jakub Karwatka <jakubkarwatka@gmail.com>
2019-04-14 21:58:54 +0200

    add persistedReducer to store, add methods for loading and saving state

--- src/js/store.js
+++ src/js/store.js
@@ -1,16 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers/rootReducer';
 import rootSaga from './sagas/sagas';
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
 
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  rootReducer,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
   composeEnhancers(applyMiddleware(sagaMiddleware))
 );
-
+export const persistor = persistStore(store);
 sagaMiddleware.run(rootSaga);
-
-export default store;
